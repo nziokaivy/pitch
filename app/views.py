5f69187a41348e52ec9b12c7efafe592efc11d58
nziokaivy <nziokaivy@gmail.com>
2019-02-07 16:25:13 +0300

    add title content blocks

--- app/views.py
+++ app/views.py
@@ -1,11 +1,27 @@
 from flask import render_template
 from app import app
 
+posts = [
+    {
+        'author': 'Ivy Mwende',
+        'title' : 'Pitch post 1',
+        'content': 'First pitch content',
+        'date_posted': 'April 20, 2018'
+    },
+
+    {
+        'author': 'John Doe',
+        'title' : 'Pitch post 2',
+        'content': 'Second pitch content',
+        'date_posted': 'April 21, 2018'
+    },
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)    
+    return render_template('home.html', posts=posts, title='Home')    
 
