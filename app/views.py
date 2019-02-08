0857a2ae75c37f9a424c05c99e511d4527a87dcc
nziokaivy <nziokaivy@gmail.com>
2019-02-08 11:33:07 +0300

    add login page form fieldsets

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for, flash
+from flask import render_template, url_for, flash, redirect
 from app import app
 from forms import RegistrationForm, LoginForm
 
@@ -34,7 +34,8 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!')
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)    
 
 @app.route('/login')
