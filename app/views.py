2e7dba2c0eb6f77051000b7dfbc96b1bba0a4b95
nziokaivy <nziokaivy@gmail.com>
2019-02-08 11:05:19 +0300

    add methods get and post to the registrtation form to allow form submsission

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, url_for
-from forms import RegistrationForm, LoginForm
+
+from flask import render_template, url_for, flash
 from app import app
+from forms import RegistrationForm, LoginForm
+
 
 app.config['SECRET_KEY'] = 'BREAD'
 
@@ -28,12 +30,17 @@ def index():
 def home():
     return render_template('home.html', posts=posts, title='Home')    
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("")
     return render_template('register.html', title='Register', form=form)    
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)    
+
+if __name__ == '__main__':
+    app.run(debug = True)
